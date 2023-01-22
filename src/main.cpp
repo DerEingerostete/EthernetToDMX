ed9d1d584ceca9c7a151370776a2b062bbc9c7d6
DerEingerostete <contact@dereingerostete.dev>
2023-01-22 19:49:08 +0100

    Update license

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2022 - 2023 * DerEingerostete
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ */
+
 #include <DmxSimple.h>
 #include <Artnet.h> //Only supports Ethernet
 #include <EasySevenSeg.h>
